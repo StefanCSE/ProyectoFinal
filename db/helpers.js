dab60b2d418d7cc33485926e6e9ce89172625e91
Stefan CSE <stefankeyboard2002@gmail.com>
2026-03-22 10:44:30 +0100

    Support db for vercel

--- db/helpers.js
+++ db/helpers.js
@@ -1,16 +1,38 @@
 const fs = require("fs");
 const path = require("path");
+const os = require("os");
 
-const PRODUCTS_FILE = path.join(__dirname, "products.json");
-const CART_FILE = path.join(__dirname, "cart.json");
+const isVercel = process.env.VERCEL === "1";
 
-// Lee un archivo JSON y devuelve su contenido como objeto
+const PRODUCTS_FILE = isVercel 
+  ? path.join(os.tmpdir(), "products.json") 
+  : path.join(__dirname, "products.json");
+
+const CART_FILE = isVercel 
+  ? path.join(os.tmpdir(), "cart.json") 
+  : path.join(__dirname, "cart.json");
+
+function initFiles() {
+  if (isVercel) {
+    if (!fs.existsSync(PRODUCTS_FILE)) {
+      const baseProducts = fs.readFileSync(path.join(__dirname, "products.json"), "utf-8");
+      fs.writeFileSync(PRODUCTS_FILE, baseProducts);
+    }
+    if (!fs.existsSync(CART_FILE)) {
+      const baseCart = fs.readFileSync(path.join(__dirname, "cart.json"), "utf-8");
+      fs.writeFileSync(CART_FILE, baseCart);
+    }
+  }
+}
+
+initFiles();
+
 function readJSON(filePath) {
+  if (!fs.existsSync(filePath)) return []; 
   const data = fs.readFileSync(filePath, "utf-8");
   return JSON.parse(data);
 }
 
-// Escribe un objeto en un archivo JSON
 function writeJSON(filePath, data) {
   fs.writeFileSync(filePath, JSON.stringify(data, null, 2));
 }
